5e82ab4935fdf5d548360410d4265fbd922ec21d
Arturo Rosales <arturoi_rosales@hotmail.com>
2020-06-03 16:04:15 -0500

    sidebar actualizado

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
       <ul class="nav flex-column">
         <li class="nav-item">
           <a class="nav-link" href="{{ route('dashboard.index') }}">
-            <i class="fas fa-tachometer-alt feather text-secondary'"></i>
+            <i class="fas fa-tachometer-alt feather"></i>
             Dashboard
           </a>
         </li>
@@ -12,13 +12,13 @@
           </h6>
         <li class="nav-item">
           <a class="nav-link" href="{{ route('app.pages') }}">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
-            Blog
+            <i class="far fa-file-alt feather"></i>
+            Noticias
           </a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{ route('app.pages') }}">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
+            <i class="fas fa-file-alt feather"></i>
             Contenido de las páginas
           </a>
         </li>
@@ -27,31 +27,31 @@
           </h6>
         <li class="nav-item">
           <a class="nav-link" href="{{ route('article.index') }}">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+            <i class="fas fa-store feather"></i>
             Todos los productos
           </a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{ route('article.index') }}">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+            <i class="fas fa-boxes feather"></i>
             Todos las categorias
           </a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{ route('article.index') }}">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+            <i class="fas fa-tags feather"></i>
             Todos las marcas
           </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bar-chart-2"><line x1="18" y1="20" x2="18" y2="10"></line><line x1="12" y1="20" x2="12" y2="4"></line><line x1="6" y1="20" x2="6" y2="14"></line></svg>
+              <i class="fas fa-toolbox feather"></i>
               Aplicaciones
             </a>
           </li>
         <li class="nav-item">
             <a class="nav-link" href="#">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bar-chart-2"><line x1="18" y1="20" x2="18" y2="10"></line><line x1="12" y1="20" x2="12" y2="4"></line><line x1="6" y1="20" x2="6" y2="14"></line></svg>
+              <i class="fas fa-file-download feather"></i>
               Descargas
             </a>
           </li>
@@ -61,7 +61,7 @@
           </h6>
         <li class="nav-item">
           <a class="nav-link" href="#">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+            <i class="fas fa-users feather"></i>
             Todos Usuarios
           </a>
         </li>
@@ -71,14 +71,8 @@
           </h6>
         <li class="nav-item">
           <a class="nav-link" href="#">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height ="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layers"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
+            <i class="fas fa-store-alt feather"></i>
             Sucursales
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height ="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layers"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
-            Zonas
           </a>
         </li>
       </ul>
