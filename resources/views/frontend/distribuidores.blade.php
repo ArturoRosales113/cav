950c22cd9ffb2c6b707e156efa041abe7cfc0923
Arturo Rosales <arturoi_rosales@hotmail.com>
2020-06-10 18:59:24 -0500

    front para proyectos y distribuidores

--- resources/views/frontend/distribuidores.blade.php
+++ resources/views/frontend/distribuidores.blade.php
@@ -1,1 +1,45 @@
 @extends('frontend.layouts.app')
+
+@section('content')
+<div class="row mt-5 py-5 bg1">
+    <div class="col text-center text-white py-3">
+        <h3>Distribuidores</h3>
+    </div>
+</div>
+
+<div class="row justify-content-center py-5">
+    <div class="col-10 col-lg-8">
+        <ul>
+            <li>
+                <h4 class="bold">¿CÓMO PUEDO SER DISTRIBUIDOR?</h4>
+                <p>REALIZANDO COMPRAS DE FORMA PERIÓDICA Y TOMANDO UN CURSO DE CAPACITACIÓN GRATUITO CON NUESTROS INGENIEROS.</p>
+            </li>
+            <li>
+                <h4 class="bold">¿INSTALAN PUERTAS AUTOMÁTICAS ES EN TODA LA REPÚBLICA?</h4>
+                <p>SÍ, INSTALAMOS PUERTAS EN TODA LA REPÚBLICA Y CENTROAMÉRICA</p>
+            </li>
+            <li>
+                <h4 class="bold">¿CUÁLES DEBEN SER LAS CARACTERÍSTICAS DE UN PORTÓN PARA PODERLO AUTOMATIZAR?</h4>
+                <p>DEPENDE DEL TIPO DE APERTURA DEL PORTÓN ASÍ COMO TAMBIÉN DEBE CONTAR CON LOS HERRAJES DE ACUERDO AL PESO Y EL
+                    TAMAÑO DEL MISMO.</p>
+            </li>
+            <li>
+                <h4 class="bold">¿CON QUIÉN ME PUEDO COMUNICAR PARA UNA REPARACIÓN?</h4>
+                <p>LLAMAR AL 01800 Y TOMAREMOS SU REPORTE DE IGUAL MANERA LE DAREMOS TIEMPO DE RESPUESTA</p>
+            </li>
+            <li>
+                <h4 class="bold">¿CÓMO APLICA LA GARANTÍA?</h4>
+                <p>ÉSTA SOLO APLICA EN DEFECTOS DE FABRICACIÓN, NO ES VÁLIDA CUANDO SEA POR VARIACIONES DE VOLTAJE, EN CASO DE SER
+                    DISTRIBUIDOR PUEDE ENVIAR LA PIEZA Y LE DAMOS UN DIAGNOSTICO EN EL DEPARTAMENTO DE MANTENIMIENTO UNA VEZ REPARADA
+                    SE ENVÍA DE REGRESO POR MEDIO DE PAQUETERÍA EN MODO OCURRE POR COBRAR.</p>
+            </li>
+            <li>
+                <h4 class="bold">¿CUÁL ES EL TIEMPO PROMEDIO DE UN ENVIÓ DE REFACCIONES?</h4>
+                <p>EN 24 HORAS SE EMBARCA Y TARDE UN PROMEDIO DE 2 A 3 DÍAS DEPENDIENDO DEL TIEMPO DE ENTREGA DE LA PAQUETERÍA Y LA
+                    ACCESIBILIDAD DEL LUGAR, MANEJAMOS LA PAQUETERÍA DE SU CONFIANZA SIEMPRE Y CUANDO TENGA OFICINA EN VERACRUZ O
+                    EN LAS SUCURSALES.</p>
+            </li>
+        </ul>
+    </div>
+</div>
+@endsection
--- resources/views/frontend/proyectos.blade.php
+++ resources/views/frontend/proyectos.blade.php
@@ -3,26 +3,248 @@
 
 @section('content')
 <div class="row">
-    <div class="col">
+    <div class="col p-0">
         <img src="{{ asset('img/as.jpg') }}" class="img-fluid" alt="">
     </div>
 </div>
-<div class="row">
-    <div class="col-lg-3">
-        <ul class="list-group">
-            <li class="list-group-item">Cras justo odio</li>
-            <li class="list-group-item">Dapibus ac facilisis in</li>
-            <li class="list-group-item">Morbi leo risus</li>
-            <li class="list-group-item">Porta ac consectetur ac</li>
-            <li class="list-group-item">Vestibulum at eros</li>
-          </ul>
+<div class="row py-5">
 
-          <a href="{{ route('front.contacto') }}" class="">
-              Contactanos
-          </a>
-    </div>
-    <div class="col-lg-9">
+    <div class="nav flex-column nav-pills col-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+        <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Hospitales</a>
+        <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Hotel</a>
+        <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Autoservicio</a>
+        <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Industria</a>
+      </div>
 
-    </div>
+      <div class="tab-content col-9" id="v-pills-tabContent">
+        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="row">
+                            <div class="col-6">
+                                <img src="{{ asset('img/as.jpg') }}" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6">
+                                <small>Junio 2018</small> <br>
+                                <h6>Aeropuerto de la ciudad de puebla</h6>
+                                <button class="btn bg1 text-white">Ficha Técnica</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                                                <div class="row">
+                            <div class="col-6">
+                                <img src="{{ asset('img/as.jpg') }}" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6">
+                                <small>Junio 2018</small> <br>
+                                <h6>Aeropuerto de la ciudad de puebla</h6>
+                                <button class="btn bg1 text-white">Ficha Técnica</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="row py-5">
+                    <div class="col-6">
+                        <div class="row">
+                            <div class="col-6">
+                                <img src="{{ asset('img/as.jpg') }}" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6">
+                                <small>Junio 2018</small> <br>
+                                <h6>Aeropuerto de la ciudad de puebla</h6>
+                                <button class="btn bg1 text-white">Ficha Técnica</button>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="col-6">
+                           <div class="row">
+                            <div class="col-6">
+                                <img src="{{ asset('img/as.jpg') }}" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6">
+                                <small>Junio 2018</small> <br>
+                                <h6>Aeropuerto de la ciudad de puebla</h6>
+                                <button class="btn bg1 text-white">Ficha Técnica</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="row">
+                            <div class="col-6">
+                                <img src="{{ asset('img/as.jpg') }}" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6">
+                                <small>Junio 2018</small> <br>
+                                <h6>Aeropuerto de la ciudad de puebla</h6>
+                                <button class="btn bg1 text-white">Ficha Técnica</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                                                <div class="row">
+                            <div class="col-6">
+                                <img src="{{ asset('img/as.jpg') }}" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6">
+                                <small>Junio 2018</small> <br>
+                                <h6>Aeropuerto de la ciudad de puebla</h6>
+                                <button class="btn bg1 text-white">Ficha Técnica</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="row py-5">
+                    <div class="col-6">
+                        <div class="row">
+                            <div class="col-6">
+                                <img src="{{ asset('img/as.jpg') }}" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6">
+                                <small>Junio 2018</small> <br>
+                                <h6>Aeropuerto de la ciudad de puebla</h6>
+                                <button class="btn bg1 text-white">Ficha Técnica</button>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="col-6">
+                           <div class="row">
+                            <div class="col-6">
+                                <img src="{{ asset('img/as.jpg') }}" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6">
+                                <small>Junio 2018</small> <br>
+                                <h6>Aeropuerto de la ciudad de puebla</h6>
+                                <button class="btn bg1 text-white">Ficha Técnica</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="row">
+                            <div class="col-6">
+                                <img src="{{ asset('img/as.jpg') }}" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6">
+                                <small>Junio 2018</small> <br>
+                                <h6>Aeropuerto de la ciudad de puebla</h6>
+                                <button class="btn bg1 text-white">Ficha Técnica</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                                                <div class="row">
+                            <div class="col-6">
+                                <img src="{{ asset('img/as.jpg') }}" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6">
+                                <small>Junio 2018</small> <br>
+                                <h6>Aeropuerto de la ciudad de puebla</h6>
+                                <button class="btn bg1 text-white">Ficha Técnica</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="row py-5">
+                    <div class="col-6">
+                        <div class="row">
+                            <div class="col-6">
+                                <img src="{{ asset('img/as.jpg') }}" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6">
+                                <small>Junio 2018</small> <br>
+                                <h6>Aeropuerto de la ciudad de puebla</h6>
+                                <button class="btn bg1 text-white">Ficha Técnica</button>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="col-6">
+                           <div class="row">
+                            <div class="col-6">
+                                <img src="{{ asset('img/as.jpg') }}" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6">
+                                <small>Junio 2018</small> <br>
+                                <h6>Aeropuerto de la ciudad de puebla</h6>
+                                <button class="btn bg1 text-white">Ficha Técnica</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="row">
+                            <div class="col-6">
+                                <img src="{{ asset('img/as.jpg') }}" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6">
+                                <small>Junio 2018</small> <br>
+                                <h6>Aeropuerto de la ciudad de puebla</h6>
+                                <button class="btn bg1 text-white">Ficha Técnica</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                                                <div class="row">
+                            <div class="col-6">
+                                <img src="{{ asset('img/as.jpg') }}" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6">
+                                <small>Junio 2018</small> <br>
+                                <h6>Aeropuerto de la ciudad de puebla</h6>
+                                <button class="btn bg1 text-white">Ficha Técnica</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="row py-5">
+                    <div class="col-6">
+                        <div class="row">
+                            <div class="col-6">
+                                <img src="{{ asset('img/as.jpg') }}" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6">
+                                <small>Junio 2018</small> <br>
+                                <h6>Aeropuerto de la ciudad de puebla</h6>
+                                <button class="btn bg1 text-white">Ficha Técnica</button>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="col-6">
+                           <div class="row">
+                            <div class="col-6">
+                                <img src="{{ asset('img/as.jpg') }}" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-6">
+                                <small>Junio 2018</small> <br>
+                                <h6>Aeropuerto de la ciudad de puebla</h6>
+                                <button class="btn bg1 text-white">Ficha Técnica</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
 </div>
 @endsection
